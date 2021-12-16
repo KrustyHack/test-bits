4d55a58815ce4c72611da4e650c61d18c08e195f
KrustyHack <KrustyHack@users.noreply.github.com>
2021-12-16 12:27:10 +0100

    Update

--- redis.tf
+++ redis.tf
@@ -1,5 +1,5 @@
-resource "google_redis_instance" "test-ws" {
-  name           = "test-ws"
-  memory_size_gb = 1
-  region         = local.region
-}
+# resource "google_redis_instance" "test-ws" {
+#   name           = "test-ws"
+#   memory_size_gb = 1
+#   region         = local.region
+# }
